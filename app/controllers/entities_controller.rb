641d7f1e70abb55a20310990c42fafee171981e4
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-02-09 16:35:55 +0200

    app: change entity index action on work with jqgrid

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,7 +1,14 @@
 class EntitiesController < ApplicationController
   
   def index
-    @entities = Entity.all
+    @columns = ['tag', 'id']
+    @entities = Entity.paginate(
+      :page     => params[:page],
+      :per_page => params[:rows],
+      :order    => order_by_from_params(params))
+    if request.xhr?
+      render :json => json_for_jqgrid(@entities, @columns)
+    end
   end
 
   def new
