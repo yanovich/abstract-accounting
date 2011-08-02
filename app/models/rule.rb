eec8e89248defd401e91b04afc53043c20988e44
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-08-02 16:40:55 +0300

    model: add rule
    Signed-off-by: Sergey Gaychuk <sergey.gaychuk@gmail.com>
    Signed-off-by: Sergey Yanovich <ynvich@gmail.com>

---
+++ app/models/rule.rb
@@ -0,0 +1,17 @@
+# Copyright (C) 2011 Sergey Yanovich <ynvich@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Please see ./COPYING for details
+
+class Rule < ActiveRecord::Base
+  validates :deal_id, :from_id, :to_id, :rate, :presence => true
+  validates_inclusion_of :fact_side, :in => [ true, false ]
+  validates_inclusion_of :change_side, :in => [ true, false ]
+  belongs_to :deal
+  belongs_to :from, :class_name => "Deal"
+  belongs_to :to, :class_name => "Deal"
+end
