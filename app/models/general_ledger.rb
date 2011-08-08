bfc0dfba3a23b3a85e6edcb76dbcf05e37c77386
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-08-08 11:41:53 +0300

    model: add general ledger
    Signed-off-by: Sergey Gaychuk <sergey.gaychuk@gmail.com>
    Signed-off-by: Sergey Yanovich <ynvich@gmail.com>

---
+++ app/models/general_ledger.rb
@@ -0,0 +1,14 @@
+# Copyright (C) 2011 Sergey Yanovich <ynvich@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Please see ./COPYING for details
+
+class GeneralLedger < Array
+  def initialize
+    Txn.all.each { |i| self << i }
+  end
+end
