fffad275d61d7debeee78bea967a3c50014ef1f8
Alexey Shepelev <al.shepelev@gmail.com>
2011-11-25 18:07:45 +0300

    test: migrate entity test unit to rspec

---
+++ spec/models/entity_spec.rb
@@ -0,0 +1,18 @@
+# Copyright (C) 2011 Sergey Yanovich <ynvich@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Please see ./COPYING for details
+
+require 'spec_helper'
+
+describe Entity do
+  it "entity" do
+    Factory(:entity)
+    should validate_presence_of :tag
+    should validate_uniqueness_of :tag
+  end
+end
