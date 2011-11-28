4a2e0e9035c9e01ed7ba4229b589be65861ce1b5
Alexey Shepelev <al.shepelev@gmail.com>
2011-11-28 12:02:05 +0300

    model: speed up rspec by change database cleaner strategy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,10 +46,6 @@ Spork.prefork do
     config.filter_run :focus => true
     config.run_all_when_everything_filtered = true
 
-    config.before(:suite) do
-      DatabaseCleaner.strategy = :truncation
-    end
-
     config.before(:each) do
       DatabaseCleaner.start
     end
