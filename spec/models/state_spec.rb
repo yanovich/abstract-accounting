aecaf6daa13cd76db062643e42e363034dde3b58
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-11-28 13:30:25 +0300

    test: convert state Test::Unit to rspec

---
+++ spec/models/state_spec.rb
@@ -0,0 +1,24 @@
+# Copyright (C) 2011 Sergey Yanovich <ynvich@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Please see ./COPYING for details
+
+require 'spec_helper'
+
+describe State do
+  it "should validate attributes" do
+    should validate_presence_of :amount
+    should validate_presence_of :start
+    should validate_presence_of :side
+    should validate_presence_of :deal_id
+    should allow_value(StateAction::PASSIVE).for(:side)
+    should allow_value(StateAction::ACTIVE).for(:side)
+    should_not allow_value("some value").for(:side)
+    should_not allow_value(22).for(:side)
+    should belong_to :deal
+  end
+end
