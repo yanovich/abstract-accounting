0d72155937b989f7a66d0724c948c1965bfd0498
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-12-07 16:47:26 +0300

    model: add permissions for manage all objects to admin user
    Signed-off-by: Sergey Gaychuk <sergey.gaychuk@gmail.com>

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,5 +2,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :manage, :all if user && user.root?
   end
 end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,19 @@
+# Copyright (C) 2011 Sergey Yanovich <ynvich@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Please see ./COPYING for details
+
+require 'spec_helper'
+require "cancan/matchers"
+
+describe Ability do
+  it "should have next behaviour" do
+    Ability.new(Factory(:user)).should_not be_able_to(:manage, :all)
+    Ability.new(nil).should_not be_able_to(:manage, :all)
+    Ability.new(RootUser.new).should be_able_to(:manage, :all)
+  end
+end
