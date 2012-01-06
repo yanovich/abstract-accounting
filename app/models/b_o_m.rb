898442f86e6d6e0759ec6a301118317631c8550b
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2012-01-06 12:26:32 +0300

    app: add BoM

---
+++ app/models/b_o_m.rb
@@ -0,0 +1,15 @@
+# Copyright (C) 2011 Sergey Yanovich <ynvich@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Please see ./COPYING for details
+
+class BoM < ActiveRecord::Base
+  has_paper_trail
+
+  validates_presence_of :resource_id
+  belongs_to :resource, :class_name => 'Asset'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111129135448) do
+ActiveRecord::Schema.define(:version => 20120106085248) do
 
   create_table "assets", :force => true do |t|
     t.string "tag"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(:version => 20111129135448) do
   end
 
   add_index "balances", ["deal_id", "start"], :name => "index_balances_on_deal_id_and_start", :unique => true
+
+  create_table "bo_ms", :force => true do |t|
+    t.integer "resource_id"
+  end
+
+  add_index "bo_ms", ["resource_id"], :name => "index_bo_ms_on_resource_id"
 
   create_table "charts", :force => true do |t|
     t.integer "currency_id"
